2a3690bc919aeed3d3f8ce9f4b9fe3ed49157b3a
Isak Snaer Sigmarsson <isaksnaer@gmail.com>
2025-04-07 21:16:59 +0000

    added search filter functionality to spell_script.js, changed spells from being contained in a list to being contained in a card template

--- spell_script.js
+++ spell_script.js
@@ -1,16 +1,40 @@
-const spellList = document.getElementById("spell-list");
+const spellCardTemplate = document.querySelector("[data-spell-template]");
+const spellCardContainer = document.querySelector("[data-spell-cards-container]");
+const spellSearchInput = document.querySelector("[data-search]");
+
 const apiString = "https://www.dnd5eapi.co/api/spells";
+
+let spells = []
+
+spellSearchInput.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase()
+    
+    spells.forEach(spell => {
+        const spellName = spell.name.toLowerCase()
+        if(spellName.includes(value)) {
+            spell.element.classList.remove("hide")
+        } else {
+            spell.element.classList.add("hide")
+        }
+    })
+})
 
 fetch(apiString)
     .then(response => response.json())
     .then(data => {
-        data.results.forEach(spell => {
-            const listItem = document.createElement("li");
-            listItem.textContent = spell.name;
-            listItem.addEventListener("click", () => getSpellDetails(spell.url));
-            spellList.appendChild(listItem);
-        });
+        spells = data.results.map(spell => {
+            const spellCard = spellCardTemplate.content.cloneNode(true)
+            const spellCardElement = spellCard.querySelector(".spell-card")
+            const spellHeader = spellCard.querySelector("[data-header]")
+            //const spellBody = spellCard.querySelector("[spell-body]")
+            spellHeader.textContent = spell.name
+            //spellBody.textContent = spell.description
+            // TODO: populate body with spell details
+            spellCardContainer.append(spellCardElement)
+            return { name: spell.name, element: spellCardElement }
+        })
     })
+
 
 function getSpellDetails(url) {
     fetch(`https://www.dnd5eapi.co${url}`)
